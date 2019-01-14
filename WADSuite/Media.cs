1f3baa10ebbdd8b38f8923550774eb07e514d66e
Hassan Uraizee <hauraize@microsoft.com>
2019-01-13 17:27:49 -0800

    Added WinAppDriver test suite.

---
+++ WADSuite/Media.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace UWPControls
+{
+    [TestClass]
+    public class Media : Test_Base
+    {
+         private static WindowsElement mediaElement1 = null;
+        private static WindowsElement mediaElement2 = null;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            //NavigateTo("Selection and picker controls", "RadioButton");
+
+            var buttonTab = session.FindElementByName("Media");
+            buttonTab.Click();
+            var button = session.FindElementByName("MediaElement");
+            button.Click();
+
+
+            var mediaElements = session.FindElementsByClassName("MediaElement");
+            Assert.IsTrue(mediaElements.Count >= 2);
+            mediaElement1 = mediaElements[0];
+            mediaElement2 = mediaElements[1];
+            Assert.IsNotNull(mediaElement1);
+            Assert.IsNotNull(mediaElement2);
+            Thread.Sleep(800);
+
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        public void PlayMedia()
+        {
+            WindowsElement play = session.FindElementByAccessibilityId("PlayPauseButton");
+            Assert.IsNotNull(play);
+            Assert.IsNotNull(session.FindElementByAccessibilityId("svPanel"));
+            play.Click();
+       
+        }
+        [TestMethod]
+        [Priority(1)]
+        public void FullWindow()
+        {
+            WindowsElement fullWindow = session.FindElementByAccessibilityId("FullWindowButton");
+            Assert.IsNotNull(fullWindow);
+           // Thread.Sleep(500);
+            //should replace with implicit wait in future. 
+            fullWindow.Click();
+            Thread.Sleep(800);
+            Assert.IsNotNull(session.FindElementByClassName("FullWindowMedia"));
+            fullWindow.Click();
+            Thread.Sleep(1000);
+        }
+    }
+
+
+   
+    [TestClass]
+    public class PersonPicture : Test_Base
+    {
+        private static WindowsElement mediaElement1 = null;
+        private static WindowsElement mediaElement2 = null;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            //NavigateTo("Selection and picker controls", "RadioButton");
+
+            var buttonTab = session.FindElementByName("Media");
+            buttonTab.Click();
+            var button = session.FindElementByName("PersonPicture");
+            button.Click();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        public void PlayMedia()
+        {
+
+        //    var button = session.FindElementByName("PersonPicture");
+
+        }
+
+    }
+    
+}
